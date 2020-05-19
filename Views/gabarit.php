7cf1f05c57fb03442f8863b5ad9e952e366cb0d6
Asuna-flipe <54886043+Asuna-flipe@users.noreply.github.com>
2020-05-19 02:07:52 +0000

    Create gabarit.php

---
+++ Views/gabarit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style/style.css" />
+        <title><?= $title ?></title>   <!-- it's a specific element of the title link-->
+    </head>
+<body>
+<div id="global">
+<?= $content ?>         <!-- it's a specific element of the content of each page -->
+</div>
+</body>
+</html>
